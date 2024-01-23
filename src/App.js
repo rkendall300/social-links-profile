739d4ef60d06334f29d415823fff75a924f25373
Ryan Kendall <ryekendallion@gmail.com>
2024-01-23 02:10:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import avatarIcon from './assets/images/avatar-jessica.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="backdrop">
+        <div id="card">
+          <div id="avatar-icon-container">
+            <img src={avatarIcon} alt="avatar" id="avatar-icon"></img>
+          </div>
+          <div id="info">
+            <h1 id="name">Jessica Randall</h1>
+            <p id="location">London, United Kingdom</p>
+            <p id="description">"Front-end developer and avid reader."</p>
+            <div id="button-container">
+              <button className="social-link" id="github">GitHub</button>
+              <button className="social-link" id="fm">Frontend Mentor</button>
+              <button className="social-link" id="linkedin">LinkedIn</button>
+              <button className="social-link" id="twitter">Twitter</button>
+              <button className="social-link" id="instagram">Instagram</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
